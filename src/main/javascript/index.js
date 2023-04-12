f2c967387a6c23da6209deeb7066afff9e9b5636
Hexatonix72 <33948652+Hexatonix72@users.noreply.github.com>
2023-04-11 23:09:15 -0400

    Getting everything correct for port call

--- src/main/javascript/index.js
+++ src/main/javascript/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
