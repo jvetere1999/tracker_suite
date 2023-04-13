d65a786185d0af892ae3ff97f4f47d8df9d4f1f1
Hexatonix72 <brk05794@sjfc.edu>
2023-04-13 13:54:36 -0400

    update CreateEvent.jsx and add EventButton.jsx
    add button to call port 3001 onclick

--- src/main/react/web/frontpage/tracker_suite_app/src/interior/pages/CreateEvent.jsx
+++ src/main/react/web/frontpage/tracker_suite_app/src/interior/pages/CreateEvent.jsx
@@ -1,96 +1,39 @@
 //import React from 'react'
 import { Header, Button, Event } from '../components';
 import React, { useState } from "react";
+import { useStateContext } from '../contexts/ContextProvider';
 import ReactDOM from "react-dom";
 import TimePicker from 'react-time-picker';
 import { Link } from "react-router-dom";
-import { Item } from '@syncfusion/ej2/splitbuttons';
-import { useStateContext } from '../contexts/ContextProvider';
+import EventButton from '../components/EventButton';
 
 
+  const CreateEvent = () => {
+    const { currentColor } = useStateContext();
+    const [eventName, setEventName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
+  const handleEventNameChange = (e) => {
+    setEventName(e.target.value);
+  };
 
-
-const CreateEvent = () => {
-  const { currentColor } = useStateContext();
-  const [timeStart, onChange1] = useState('10:00');
-  const [timeEnd, onChange2] = useState('11:00');
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (eventName.trim() === "") {
+      setErrorMessage("Please enter an event name.");
+    } else {
+      // Handle event submission logic here
+    }
+  };
 
   return (
     <div className=' m-2 md:m-10 p-2 md:p-10 drop-shadow-2xl dark:text-gray-200 dark:bg-main-dark-bg bg-white rounded-3xl'>
       <Header category="Page" title="Create Event" />
       <main className='mt-8 max-w-md'>
-        <form className='grid grid-cols-1 gap-6'>
-          <label class="eventName">
-            <span class="text-gray-700 dark:text-gray-200">Event Name:</span>
-            <input type="text" class="
-              p-2
-              mt-1 
-              block 
-              w-full 
-              rounded-md
-              drop-shadow-md
-             dark:text-gray-700
-             bg-gray-200
-             border-gray-300
-             focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder></input>
-          </label>
-
-          <label class="eventDate">
-            <span class="text-gray-700 dark:text-gray-200">When is your Event?</span>
-            <input type="date" class="
-              p-2
-              mt-1
-              block
-              w-full
-              drop-shadow-md
-              rounded-md
-              dark:text-gray-700
-              bg-gray-200
-              border-gray-300
-              focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder="mm/dd/yyyy"></input>
-          </label>
-
-          <label class="eventStartTime">
-            <span class="text-gray-700 dark:text-gray-200">What time does your event Start?</span>
-            <input type="time" class="
-              p-2
-              mt-1
-              block
-              w-full
-              drop-shadow-md
-              rounded-md
-              dark:text-gray-700
-              bg-gray-200
-              border-gray-300
-              focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder=''></input>
-          </label>
-
-          <label class="eventEndTime">
-            <span class="text-gray-700 dark:text-gray-200">What time does your event end?</span>
-            <input type="time" class="
-              p-2
-              mt-1
-              block
-              w-full
-              rounded-md
-              drop-shadow-md
-              bg-gray-200
-              border-gray-300
-              dark:text-gray-700
-              focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder='Event Name'></input>
-          </label>
-
-          <label class="eventName">
-            <span class="text-gray-700 dark:text-gray-200">Where will your event be held?</span>
-            <input type="text" class="
+        <form className='grid grid-cols-1 gap-6' onSubmit={handleSubmit}>
+          <label className="eventName">
+            <span className="text-gray-700 dark:text-gray-200">Event Name:</span>
+            <input type="text" className="
               p-2
               mt-1
               block
@@ -101,12 +44,64 @@ const CreateEvent = () => {
               bg-gray-200
               border-gray-300
               focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder></input>
+              " value={eventName}
+              onChange={handleEventNameChange}></input>
+              {errorMessage && (
+              <p className="text-red-500 text-xs italic">{errorMessage}</p>
+            )}
           </label>
 
-          <label class="eventrepeat">
-            <span class="text-gray-700 dark:text-gray-200">Does your event repeat?</span>
-            <select class="
+          <label className="eventDate">
+            <span className="text-gray-700 dark:text-gray-200">When is your Event?</span>
+            <input type="date" className="
+              p-2
+              mt-1
+              block
+              w-full
+              drop-shadow-md
+              rounded-md
+              dark:text-gray-700
+              bg-gray-200
+              border-gray-300
+              focus:border-gray-500 focus:bg-white focus:ring-0
+              "></input>
+          </label>
+
+          <label className="eventStartTime">
+            <span className="text-gray-700 dark:text-gray-200">What time does your event Start?</span>
+            <input type="time" className="
+              p-2
+              mt-1
+              block
+              w-full
+              drop-shadow-md
+              rounded-md
+              dark:text-gray-700
+              bg-gray-200
+              border-gray-300
+              focus:border-gray-500 focus:bg-white focus:ring-0
+              "></input>
+          </label>
+
+          <label className="eventEndTime">
+            <span className="text-gray-700 dark:text-gray-200">What time does your event end?</span>
+            <input type="time" className="
+              p-2
+              mt-1
+              block
+              w-full
+              rounded-md
+              drop-shadow-md
+              bg-gray-200
+              border-gray-300
+              dark:text-gray-700
+              focus:border-gray-500 focus:bg-white focus:ring-0
+              "></input>
+          </label>
+
+          <label className="eventLocation">
+            <span className="text-gray-700 dark:text-gray-200">Where will your event be held?</span>
+            <input type="text" className="
               p-2
               mt-1
               block
@@ -117,7 +112,23 @@ const CreateEvent = () => {
               bg-gray-200
               border-gray-300
               focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder='Location'>
+              "></input>
+          </label>
+
+          <label className="eventRepeat">
+            <span className="text-gray-700 dark:text-gray-200">Does your event repeat?</span>
+            <select className="
+              p-2
+              mt-1
+              block
+              w-full
+              rounded-md
+              drop-shadow-md
+              dark:text-gray-700
+              bg-gray-200
+              border-gray-300
+              focus:border-gray-500 focus:bg-white focus:ring-0
+              ">
                 <option></option>
                 <option>No</option>
                 <option>Daily</option>
@@ -126,9 +137,9 @@ const CreateEvent = () => {
               </select>
           </label>
           
-          <label class="eventdetails">
-            <span class="text-gray-700 dark:text-gray-200">Additional Details(For you only):</span>
-            <textarea class="
+          <label className="eventDetails">
+            <span className="text-gray-700 dark:text-gray-200">Additional Details(For you only):</span>
+            <textarea className="
               
               mt-1
               block
@@ -139,22 +150,23 @@ const CreateEvent = () => {
               bg-gray-200
               border-gray-300
               focus:border-gray-500 focus:bg-white focus:ring-0
-              " rows="5" spellcheck="true"></textarea>
+              "rows="5" spellCheck="true"></textarea>
           </label>
 
-          <Button
+          <EventButton
           color="white"
           bgColor={currentColor}
           text="Submit"
           borderRadius="10px"
           drop-shadow="md"
-          className="popup-button"/>
+          className="popup-button w-100%"
+          //onClick={() => callPort('papameter_value')}
+          type="submit"/>
+          
         </form>
       </main>
-
-
       </div>
-  )
-}
+  );
+};
 
 export default CreateEvent
