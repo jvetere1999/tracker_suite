1f89dea20c6ca4a205f64c2d7dba519a0500a5c5
Hexatonix72 <33948652+Hexatonix72@users.noreply.github.com>
2023-04-25 22:05:31 -0400

    weird updates

--- src/main/flutter/Sproject/applogin/lib/main.dart
+++ src/main/flutter/Sproject/applogin/lib/main.dart
@@ -1,112 +1,121 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(const MyApp());
- 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
- 
-  static const String _title = 'Event Edge';
- 
+import 'package:auth0_flutter/auth0_flutter.dart';
+
+import 'clock.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({Key? key}) : super(key: key);
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  Credentials? _credentials;
+
+  late Auth0 auth0;
+
+  @override
+  void initState() {
+    super.initState();
+    auth0 = Auth0('dev-ocj0rgmusw7m0hn7.us.auth0.com',
+        '4VJyOaNVpCaFte5vrucmPhgyPRRswBUP');
+  }
+
+  Future<void> _logout() async {
+    await auth0.webAuthentication();
+    setState(() {
+      _credentials = null;
+    });
+  }
+
+  void _navigateToNextPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NextPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Welcome to EventPro Mobile!',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 24,
+                foreground: Paint()
+                  ..shader = LinearGradient(
+                    colors: [Colors.blue, Colors.green],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0)),
+              ),
+            ),
+          ),
+          Clock(),
+          SizedBox(height: 16),
+          if (_credentials == null)
+            ElevatedButton(
+              onPressed: () async {
+                final credentials = await auth0.webAuthentication().login();
+
+                setState(() {
+                  _credentials = credentials;
+                });
+
+                if (_credentials != null) {
+                  _navigateToNextPage();
+                }
+              },
+              child: const Text("Log in"),
+            )
+          else
+            Column(
+              children: [
+                Text(_credentials!.accessToken),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _logout,
+                  child: const Text("Log out"),
+                ),
+              ],
+            ),
+        ],
       ),
     );
   }
 }
- 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
- 
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
- 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
- 
+
+class NextPage extends StatelessWidget {
+  const NextPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Event Pro',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 32, 40, 48),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text('Forgot Password',),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )
-            ),
-            Row(
-              children: <Widget>[
-                const Text('Does not have account?'),
-                TextButton(
-                  child: const Text(
-                    'Sign Up',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                    //signup screen
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Next Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go Back'),
+        ),
+      ),
+    );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MainView(),
+  ));
 }
