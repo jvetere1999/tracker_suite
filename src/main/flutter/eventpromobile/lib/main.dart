3e4878aa41a812832674e02a2719e06bffb89b10
Hexatonix72 <brk05794@sjfc.edu>
2023-04-27 17:44:20 -0400

    add qr_scanner dependencies to eventpromobile

--- src/main/flutter/eventpromobile/lib/main.dart
+++ src/main/flutter/eventpromobile/lib/main.dart
@@ -4,13 +4,131 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:auth0_flutter/auth0_flutter.dart';
+
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+const appScheme = 'eventpromobile';
+
 /// -----------------------------------
 ///           Profile Widget
 /// -----------------------------------
+class Profile extends StatelessWidget {
+  final Future<void> Function() logoutAction;
+  final UserProfile? user;
+
+  const Profile(this.logoutAction, this.user, {final Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 1),
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(user?.pictureUrl.toString() ?? ''),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const SizedBox(height: 16),
+                  Text('${user?.name}',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18.0,
+                      )),
+                  const SizedBox(height: 2),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await logoutAction();
+                    },
+                    child: const Text('Logout'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        Flexible(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(16),
+            ),
+            margin: EdgeInsets.all(16),
+            child: Center(
+              child: Text(
+                'New Box in the Middle',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(16),
+          ),
+          margin: EdgeInsets.all(16),
+          child: Center(
+            child: Text(
+              'New Box in the Middle',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
 
 /// -----------------------------------
 ///            Login Widget
 /// -----------------------------------
+class Login extends StatelessWidget {
+  final Future<void> Function() loginAction;
+  final String loginError;
+
+  const Login(this.loginAction, this.loginError, {final Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () async {
+            await loginAction();
+          },
+          child: const Text('Login'),
+        ),
+        Text(loginError ?? ''),
+      ],
+    );
+  }
+}
 
 /// -----------------------------------
 ///                 App
@@ -28,6 +146,9 @@ class MyApp extends StatefulWidget {
 /// -----------------------------------
 
 class _MyAppState extends State<MyApp> {
+  Credentials? _credentials;
+  late Auth0 auth0;
+
   bool isBusy = false;
   late String errorMessage;
 
@@ -40,9 +161,54 @@ class _MyAppState extends State<MyApp> {
           title: const Text('EventPro Mobile'),
         ),
         body: Center(
-          child: const Text('Implement User Authentication'),
+          child: isBusy
+              ? const CircularProgressIndicator()
+              : _credentials != null
+                  ? Profile(logoutAction, _credentials?.user)
+                  : Login(loginAction, errorMessage),
         ),
       ),
     );
+  }
+
+  Future<void> loginAction() async {
+    setState(() {
+      isBusy = true;
+      errorMessage = '';
+    });
+
+    try {
+      final Credentials credentials =
+          await auth0.webAuthentication(scheme: appScheme).login();
+
+      setState(() {
+        isBusy = false;
+        _credentials = credentials;
+      });
+    } on Exception catch (e, s) {
+      debugPrint('login error: $e - stack: $s');
+
+      setState(() {
+        isBusy = false;
+        errorMessage = e.toString();
+      });
+    }
+  }
+
+  Future<void> logoutAction() async {
+    await auth0.webAuthentication(scheme: appScheme).logout();
+
+    setState(() {
+      _credentials = null;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    auth0 = Auth0('dev-ocj0rgmusw7m0hn7.us.auth0.com',
+        'kfs3MfTcr6XcoAoxE3Fu4Iawzky5IQR1');
+    errorMessage = '';
   }
 }
